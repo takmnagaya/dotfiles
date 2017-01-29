a8932ed159ea499b2c5466dc3319084f51276186
takmnagaya <takmnagaya@gmail.com>
2017-01-29 10:46:53 +0900

    brew install symbolic link setteings

---
+++ mac_install.sh
@@ -0,0 +1,103 @@
+# symbolic link setting
+ln -sf ~/dotfiles/.oh-my-zsh .oh-my-zsh
+ln -sf ~/dotfiles/.tmux.conf .tmux.conf
+ln -sf ~/dotfiles/.vim .vim
+ln -sf ~/dotfiles/.vimrc .vimrc
+ln -sf ~/dotfiles/.zsh/ .zsh
+ln -sf ~/dotfiles/.zshrc .zshrc
+ln -sf ~/dotfiles/.zshrc .zshrc.pre-oh-my-zsh
+ln -sf ~/dotfiles/.irbrc .irbrc
+# brew install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install ack
+brew install apr
+brew install apr-util
+brew install autoconf
+brew install automake
+brew install aws-elasticbeanstalk
+brew install boost
+brew install cmake
+brew install coreutils
+brew install ctags
+brew install curl
+brew install direnv
+brew install fontconfig
+brew install freetype
+brew install gd
+brew install gdbm
+brew install git
+brew install gnu-sed
+brew install go
+brew install graphviz
+brew install heroku
+brew install heroku-toolbelt
+brew install httpd22
+brew install hub
+brew install imagemagick
+brew install jenv
+brew install jpeg
+brew install jq
+brew install libevent
+brew install libpng
+brew install libtiff
+brew install libtool
+brew install lua
+brew install lv
+brew install mysql
+brew install nginx
+brew install nkf
+brew install node
+brew install oniguruma
+brew install openssl
+brew install openssl@1.1
+brew install pcre
+brew install peco
+brew install phantomjs
+brew install pkg-config
+brew install postgresql
+brew install pyenv
+brew install python
+brew install rbenv
+brew install rbenv-gem-rehash
+brew install readline
+brew install reattach-to-user-namespace
+brew install redis
+brew install rlwrap
+brew install ruby-build
+brew install sqlite
+brew install subversion
+brew install the_silver_searcher
+brew install tig
+brew install tmux
+brew install tomcat@7
+brew install tree
+brew install utf8proc
+brew install v8
+brew install v8-315
+brew install vim
+brew install webp
+brew install wget
+brew install xz
+brew install zlib
+brew install zplug
+brew install zsh
+# brew cask install
+brew cask install adobe-reader
+brew cask install brackets
+brew cask install dropbox
+brew cask install firefox
+brew cask install flux
+brew cask install google-chrome
+brew cask install google-japanese-ime
+brew cask install java
+brew cask install java7
+brew cask install karabiner
+brew cask install kindle
+brew cask install pgadmin3
+brew cask install recordit
+brew cask install skitch
+brew cask install skype
+brew cask install sqlitebrowser
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install xtrafinder
